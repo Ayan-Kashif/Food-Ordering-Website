85863351e9375fff8a68cfa5910796b1f46de929
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-03-15 00:06:00 +0500

    Update page.js

--- app/admin/panel/page.js
+++ app/admin/panel/page.js
@@ -1143,10 +1143,10 @@ const StaffPanel = () => {
   const [expandedOrder, setExpandedOrder] = useState(null)
   const [expandedInstructions, setExpandedInstructions] = useState(null);
   const [authChecked, setAuthChecked] = useState(false)
-
+const [loading, setLoading] = useState(false)
   const checkAdminAuth = async () => {
     const adminAuthToken = localStorage.getItem("adminAuthToken");
-     const [loading, setLoading] = useState(false)
+     
           
             const handlePageLoad = () => {
               setLoading(true);
